8f057876d7d72bbd60531493817be7a36759706d
tapan savani <201952231@iiitvadodara.ac.in>
2021-02-11 08:40:41 +0530

    Lab 3 assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function check() {
+    var x = document.getElementById('p1').value;
+    var y = document.getElementById('p2').value;
+
+    if (x != y)
+        alert("Please Enter Valid Password");
+}
